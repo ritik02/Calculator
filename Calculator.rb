c9f4b91f1f2f7462e6144272ec1c37953ea473ce
Ritik Verma <ritikverma@Ritiks-MacBook-Pro.local>
2018-05-15 19:11:35 +0530

    Add Default arguments for handling cases when no number is provided

--- Calculator.rb
+++ Calculator.rb
@@ -3,19 +3,19 @@ class Calculator
    def initialize (number=0.0)
       @number=number
    end
-   def add number2
+   def add (number2=0.0)
       @number=@number+number2
       puts @number
    end
-   def subtract number2
+   def subtract (number2=0.0)
       @number=@number-number2
       puts @number
    end
-   def multiply number2
+   def multiply (number2=1.0)
       @number=@number*number2
       puts @number
    end
-   def divide number2
+   def divide (number2=1.0)
       if number2.zero?
       temp=@number
       puts "Division not performed , enter another number other than zero"
@@ -44,18 +44,22 @@ loop do
        when 'add'
           if values.length==2
              object1.add values[1].to_f
+          else object1.add
           end
        when 'subtract'
            if values.length==2
                object1.subtract values[1].to_f
+           else object1.subtract
            end
        when 'divide'
            if values.length==2
                object1.divide values[1].to_f
+           else object1.subtract
            end
        when 'multiply'
           if values.length==2
              object1.multiply values[1].to_f
+          else object1.subtract
           end
       else
        puts "Invalid Command , enter a valid one"
